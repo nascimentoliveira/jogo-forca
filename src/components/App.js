a9ea90c089503f25d4912007e2c377d8c7f366fb
Thiago Nascimento <nascimentoliveira@gmail.com>
2022-09-24 00:39:59 -0300

    feat (Game): layout/style created

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
+import styled from "styled-components";
 import Game from "./Game"
 import Letters from "./Letters"
 import Guess from "./Guess"
+import GlobalStyle from "../assets/theme/globalStyles"
 
 export default function App() {
   // === LOGIC ===
@@ -8,9 +10,35 @@ export default function App() {
   // === UI ===
   return (
     <>
-      <Game />
-      <Letters />
-      <Guess />
+      <GlobalStyle />
+      <Main>
+        <Content>
+          <Game />
+          <Letters />
+          <Guess />
+        </Content>
+      </Main>
     </>
   );
 }
+
+const Main = styled.main`
+  padding: 5%;
+  position:absolute; 
+  top:0; 
+  bottom:0; 
+  right:0; 
+  left:0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #EFEFEF;
+`;
+
+const Content = styled.article`
+	height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,10 +1,11 @@
-import gallow0 from "/src/assets/images/forca0.png";
-import gallow1 from "/src/assets/images/forca1.png";
-import gallow2 from "/src/assets/images/forca2.png";
-import gallow3 from "/src/assets/images/forca3.png";
-import gallow4 from "/src/assets/images/forca4.png";
-import gallow5 from "/src/assets/images/forca5.png";
-import gallow6 from "/src/assets/images/forca6.png";
+import styled from "styled-components";
+import gallow0 from "../assets/images/forca0.png";
+import gallow1 from "../assets/images/forca1.png";
+import gallow2 from "../assets/images/forca2.png";
+import gallow3 from "../assets/images/forca3.png";
+import gallow4 from "../assets/images/forca4.png";
+import gallow5 from "../assets/images/forca5.png";
+import gallow6 from "../assets/images/forca6.png";
 
 const images = [gallow0, gallow1, gallow2, gallow3, gallow4, gallow5, gallow6];
 
@@ -13,7 +14,77 @@ export default function Game() {
 
   // === UI ===
   return (
-    <>
-    </>
+    <GameDisplay>
+      <Scoreboard>
+        <img src={gallow6}/>
+      </Scoreboard>
+      <WordDisplay>
+        <button>SORTEAR NOVA PALAVRA</button>
+        <Match>
+          <Letter>_</Letter>
+          <Letter>_</Letter>
+          <Letter>_</Letter>
+          <Letter>_</Letter>
+          <Letter>_</Letter>
+          <Letter>_</Letter>
+        </Match>
+      </WordDisplay>
+    </GameDisplay>
   );
 }
+
+const GameDisplay = styled.figure`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+`;
+
+const Scoreboard = styled.figure`
+  width: 50%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  img {
+    max-width: 300px;
+    width: 100%;
+    height: auto;
+  }
+`;
+
+const WordDisplay = styled.section`
+  max-width: 50%;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+  button {
+    background-color: #27AE60;
+    color: #FFFFFF;
+    max-width: 150px;
+    width: 100%;
+    height: 50px;
+    border-radius: 5px;
+    border: none;
+    outline: none;
+  }
+`;
+
+const Match = styled.section`
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
+
+const Letter = styled.div`
+  margin: 0px 10px;
+  width: 30px;
+  height: 30px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
